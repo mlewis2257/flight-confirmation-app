cf02ab8515519e3f4763d28d2f846dc49a002eea
mlewis2257 <mlang.lewis@gmail.com>
2025-04-25 15:55:30 -0700

    models

---
+++ backend/models/flights.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  airline: String,
+  confirmationNumber: String,
+  passengerFirstName: String,
+  passengerLastName: String,
+  departureTime: Date,
+  checkinCompleted: { type: Boolean, default: false },
+  boardingPassUrl: String,
+});
+
+module.exports = mongoose.model("Flight", flightSchema);
