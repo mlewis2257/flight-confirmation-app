c84a06f02f2350f57272f7e621ea960cd7656b8e
mlewis2257 <mlang.lewis@gmail.com>
2025-05-29 16:01:53 -0700

    authMiddleware

--- backend/routes/flights.js
+++ backend/routes/flights.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../utils/authMiddleWare");
 
 router.post("api");
 
--- backend/utils/authMiddleWare.js
+++ backend/utils/authMiddleWare.js
@@ -1,3 +1,18 @@
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
-module.exports = (req, res, next) => {};
+const JWT = process.env.JWT;
+
+module.exports = (req, res, next) => {
+  const token = req.header("Authorication")?.replace("Bearer", "");
+  if (!token) return res.status(401).json({ msg: "Access Denied" });
+
+  try {
+    const decoded = jwt.verify(token, JWT);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Invalid Token" });
+  }
+};
