52ba8abe75ba24e07b40eefefd39df29e04d71a8
mlewis2257 <mlang.lewis@gmail.com>
2025-07-25 00:18:57 -0700

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Router } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import AddFlight from "./pages/AddFlight";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <Router></Router>;
 }
 
 export default App;
